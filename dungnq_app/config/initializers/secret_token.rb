d2a10e8161d6a73b73af9ea34f70c2001d0d7f1c
DungNQ <nguyen.quang.dung@framgia.com>
2013-09-16 16:51:46 +0700

    Initialize repository

---
+++ dungnq_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DungnqApp::Application.config.secret_key_base = '585761b2d5fc1b180a7790e8cfa552c3d8e784b231627d7a6f02de256e32ce679bbb44d009437f950e030cf0cb21d9a7247432dfc1c64422f27ae4e0f40f741f'
